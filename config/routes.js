05c2f26af15d3e43c378524ab00872ccfa848661
bluehugh2 <bluehugh2@gmail.com>
2013-07-04 11:42:04 -0400

    clear

--- config/routes.js
+++ config/routes.js
@@ -1,27 +1,28 @@
 module.exports = function(app) {
   
-  // test
-  var test = require('../controllers/test');
-  app.get('/', test.index);
-  app.get('/test', test.test);
-  app.get('/bryant', test.bryant);
-  app.post('/message', test.message);
+  /*** Website ***/
+  var web = require('../views/controller');
+  app.get('/', web.index);
+  app.get('/test', web.test);
+  app.get('/bryant', web.bryant);
+  app.post('/message', web.message);
 
+  /*** API ***/
   // users
-  var users = require('../controllers/users');
-  app.get('/users', users.list);
-  app.get('/users/:id', users.show);
-  app.post('/users', users.create);
-  app.post('/login', users.login);
+  var users = require('../api/users');
+  app.get('/api/users', users.list);
+  app.get('/api/users/:id', users.show);
+  app.post('/api/users', users.create);
+  app.post('/api/login', users.login);
   
   // zones
-  var zones = require('../controllers/zones');
+  var zones = require('../api/zones');
   app.get('/zones', zones.list);
   app.get('/zones/:id', zones.show);
   app.post('/zones', zones.create);
   
   // audio
-  var audio = require('../controllers/audio');
+  var audio = require('../api/audio');
   app.get('/audio', audio.zones);
   app.get('/audio/:id', audio.zone);
   app.post('/audio', audio.volume);

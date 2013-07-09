b5a6f38cfd77bb95d15ad4e429b0b2b0cdd8988e
Hugh Boylan <hugh@peak-server.(none)>
2013-07-09 17:51:18 -0400

    merging

--- config/connect.js
+++ config/connect.js
@@ -2,13 +2,13 @@ var config  = require('./config.json')
   , net     = require('net');
 
 //Object containing our connection to the TCP server
-var Client = function() {
-  return {
+function Client() {
+
     //Instance of our ongoing connection to the server
-    connection: new net.Socket(),
+    this.connection = new net.Socket();
     
     //Authenticate with the TCP server
-    connect: function() {
+    this.connect = function() {
       console.log('Attempting to reach the TCP server...');
       this.connection.connect(config.tcp_port, config.tcp_host, function() {
         this.write(config.tcp_key);
@@ -19,29 +19,29 @@ var Client = function() {
       });
       this.connection.on('close', function(){
         console.log('TCP Connection disconnected');
-        this.reconnect(5);
+        //reconnect(5);
         
       });
-    },
+    };
     
-    reconnect: function(attempts) {
+    this.reconnect = function(attempts) {
       setTimeout(function(){
         this.connect();
         if(!this.connection.writable)
           this.reconnect(attempts--);
       }, 1000);
-    }
+    };
     
     //Send message to the server
-    send: function(msg) {
+    this.send = function(msg) {
       this.connection.write(msg);
-    },
+    };
     
     //Query the server expecting a response
-    query: function(msg) {
+    query = function(msg) {
       this.send(msg);
-    }
-  }
+    };
+
 };
 
 exports.client = function() {

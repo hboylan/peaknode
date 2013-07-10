5dfe1ebf730c9f7acc7cbf5698b405a7ae25ee6d
bluehugh2 <bluehugh2@gmail.com>
2013-07-10 08:42:55 -0400

    tcp won't send if not connected

--- api/audio.js
+++ api/audio.js
@@ -37,12 +37,15 @@ exports.volume = function(req, res) {
     , on      = vol == 0
     , msg     = on? 'audiocontrol '+zone+' 3' : 'audiovolume '+zone+' '+vol;
   //Check we have proper params
+  if(zone == undefined)
+    return res.json({'error':'Expected parameter: zone'});
   if(zone < 1 || zone > 8)
     return res.json({'error':'Audio Zone must be 1-8'});
   if(vol < 0 || vol > 100)
-    return res.json({'error':'Volume must be 0-100'})
+    return res.json({'error':'Volume must be 0-100'});
+  
   //Tell TCP server to change volume for zone
-  // client.send(msg);
+  client.send(msg);
   console.log(msg);
   //Update db, client response
   audio.find({ zone_id:zone }).success(function(a){
--- config/connect.js
+++ config/connect.js
@@ -6,26 +6,30 @@ function Client() {
 
     //Instance of our ongoing connection to the server
     this.connection = new net.Socket();
+    this.connection.setEncoding('utf-8');
+    this.connection.on('error', function (e) {
+      if(e.code == 'ECONNREFUSED')
+        console.log('TCP server offline');
+    });
+    this.connection.on('data', function(msg){
+      console.log("TCP connection received: '"+msg+"'");
+    });
+    this.connection.on('close', function(){
+      console.log('TCP connection closed');
+      //reconnect(5);
+    });
     
-    //Authenticate with the TCP server
+    //Authenticate client with the TCP server
     this.connect = function() {
-      console.log('Attempting to reach the TCP server...');
+      console.log('Attempting to reach TCP server on port '+config.tcp_port+'...');
       this.connection.connect(config.tcp_port, config.tcp_host, function() {
         this.write(config.tcp_key);
         console.log('TCP connection successful');
       });
-      this.connection.on('data', function(d){
-        console.log(d);
-      });
-      this.connection.on('close', function(){
-        console.log('TCP Connection disconnected');
-        //reconnect(5);
-        
-      });
     };
     
     this.reconnect = function(attempts) {
-      setTimeout(function(){
+      if(attempts > 0) setTimeout(function(){
         this.connect();
         if(!this.connection.writable)
           this.reconnect(attempts--);
@@ -34,7 +38,11 @@ function Client() {
     
     //Send message to the server
     this.send = function(msg) {
-      this.connection.write(msg);
+      var conn = this.connection;
+      if(conn.writable)
+        this.connection.write(msg);
+      else
+        console.log("TCP client failed to send: '"+msg+"'");
     };
     
     //Query the server expecting a response

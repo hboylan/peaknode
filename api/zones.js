8cc0f2b40669a78e14677074ecd04399c844e018
bluehugh2 <bluehugh2@gmail.com>
2013-07-16 15:33:22 -0400

    ..

--- api/zones.js
+++ api/zones.js
@@ -2,13 +2,16 @@ var zone = require('../config/database').zone;
 
 exports.list = function(req, res) {
   zone.all().success(function(zones){
+    zones.forEach(function(z){
+      z = z.parse();
+    });
     res.json(zones);
   });
 };
 
 exports.show = function(req, res) {
   zone.find({ where:{ zoneId:req.params.id }}).success(function(zone){
-    res.json(zone);
+    res.json(zone.parse());
   });
 };
 
@@ -17,6 +20,6 @@ exports.create = function(req, res) {
     name:req.body.name,
     zoneId:req.body.zone
   }).success(function(zone){
-    res.json(zone);
+    res.json(zone.parse());
   });
 };
--- models/zone.js
+++ models/zone.js
@@ -2,5 +2,12 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define('zone', {
     name: DataTypes.STRING,
     zoneId: { type:DataTypes.INTEGER, validate:{ min:1, max:8 }},
+  }, {
+    instanceMethods:{
+      parse:function(){
+        this.id = this.updatedAt = this.createdAt = undefined;
+        return this;
+      }
+    }
   });
 };

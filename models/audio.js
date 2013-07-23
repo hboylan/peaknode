aa64b13c83fa5f7fb264d32754637db02e53d956
bluehugh2 <bluehugh2@gmail.com>
2013-07-23 12:10:07 -0400

    mute commands

--- models/audio.js
+++ models/audio.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define('audio', {
     name: DataTypes.STRING,
     source: { type:DataTypes.INTEGER, validate:{ min:1, max:8 }, defaultValue:1 },
-    state: { type:DataTypes.ENUM, values:['on', 'off', 'mute', 'unmute'], defaultValue:'off' },
+    state: { type:DataTypes.ENUM, values:['on', 'off', 'mute'], defaultValue:'off' },
     volume: { type:DataTypes.INTEGER, validate:{ min:0, max:100 }, defaultValue:0 },
   }, {
     freezeTableName: true,
@@ -14,16 +14,14 @@ module.exports = function(sequelize, DataTypes) {
       },
       
       setState:function(client, state){
-        var states  = ['off', 'on', 'mute', 'unmute'];
+        var states  = ['off', 'on', 'unmute', 'mute'];
         client.send('audiocontrol '+this.id+' '+states.indexOf(state));
         this.state = state;
       },
       
       setVolume:function(client, vol){
-        var mute    = vol == 0
-          , msg     = mute? 'audiocontrol '+this.id+' 3' : 'audiovolume '+this.id+' '+vol;
-  
-        client.send(msg);
+        // if(this.state == 'off') client.send('audiocontrol '+this.id+' 1');
+        client.send('audiovolume '+this.id+' '+vol);
         this.state = 'on';
         this.volume = parseInt(vol, 10);
       },

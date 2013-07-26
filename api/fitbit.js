96becb038fc9f3e3124febbdd55ad5077719ea7d
bluehugh2 <bluehugh2@gmail.com>
2013-07-26 19:43:22 -0400

    fitbit fix

--- api/fitbit.js
+++ api/fitbit.js
@@ -1,5 +1,9 @@
 function API(client){
-  this.auth = function(req, res){ client.authenticate(req, res) }
+  this.auth = function(req, res){
+    client.getAccessToken(req, res, function(error){
+      res.send(err, 500)
+    })
+  }
   
   this.test = function(req, res){
     client.apiCall('GET', '/user/26RNTZ/profile.json', { token:req.cookies.fitbit_client },

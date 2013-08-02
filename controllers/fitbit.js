aaa1b85d5ac57754fdd0ed3ea945f4f9a66b9607
bluehugh2 <bluehugh2@gmail.com>
2013-08-02 19:14:49 -0400

    ..

--- controllers/fitbit.js
+++ controllers/fitbit.js
@@ -22,25 +22,29 @@ function API(User, client)
     }
   }
   
-  this.auth           = function(req, res){ client.getRequestToken(req, res) }
-  this.access         = function(req, res){
-    var token     = req.query.oauth_token
-      , verifier  = req.query.oauth_verifier;
-    // Access token
-    client.oauth.getOAuthAccessToken(token, '', verifier,
-      function (error, token, secret, other) {
-        if(error) return res.status(400).json(error)
-        client.cookie(res, token, secret) //client cookie
-      
-        User.find(req.cookies.user.id).success(function(u){ //persist in db
-          if(u != undefined) u.updateAttributes({fitbit_token:token, fitbit_secret:secret}).success(function(){ res.redirect('/users') })
-        })
+  this.auth = function(req, res){
+    // Request token
+    client.oauth.getOAuthRequestToken(function (error, token, secret, authorize_url, other) {
+      if(error) return res.status(400).json({ error:'Failed to request token' })
+      res.redirect('http://www.fitbit.com/oauth/authorize?oauth_token=' + token)
     })
   }
-  this.userAction     = function(req, res){ client.userRequest(req.params.action, req, apiHandle(res)) }
-  this.userSubAction  = function(req, res){ client.userRequest(req.params.action+'/'+req.params.sub, req, apiHandle(res)) }
+  this.access = function(req, res){
+    var token = req.query.oauth_token, verifier = req.query.oauth_verifier;
+    // Access token
+    client.oauth.getOAuthAccessToken(token, '', verifier, function (error, token, secret, other){
+      if(error) return res.status(400).json(error)
+      client.cookie(res, client.serializer.stringify({token:token, secret:secret})) //client cookie
+    
+      User.find(req.cookies.user.id).success(function(u){ //persist in db
+        if(u != undefined) u.updateAttributes({fitbit_token:token, fitbit_secret:secret}).success(function(){ res.redirect('/users') })
+      })
+    })
+  }
+  this.userAction = function(req, res){ client.userRequest(req.params.action, req, apiHandle(res)) }
+  this.userSubAction = function(req, res){ client.userRequest(req.params.action+'/'+req.params.sub, req, apiHandle(res)) }
   
-  this.bodyRange   = function(req, res){ client.userRequest('body/'+req.params.sub+'/date/'+req.params.start+'/'+req.params.end, req, apiHandle(res)) }
+  this.bodyRange = function(req, res){ client.userRequest('body/'+req.params.sub+'/date/'+req.params.start+'/'+req.params.end, req, apiHandle(res)) }
 }
 
 exports.API = API;

30cc1dee49e11de8abc125ba8eb82d44e377e4c3
bluehugh2 <bluehugh2@gmail.com>
2013-08-02 15:53:32 -0400

    update

--- controllers/fitbit.js
+++ controllers/fitbit.js
@@ -1,5 +1,20 @@
-function API(client){
-  
+/*
+ * User Request Options
+ * 
+ * /profile
+ * /activities
+ * /friends
+ * /friends/leaderboard
+ * /body
+ * 
+ * /activities/:resource/date/:start/:end, [distance, calories, activeScore]
+ * /sleep/:resource/date/:start/:end [timeInBed, efficiency]
+ * /body/:resource/date/:start/:end [weight, bmi]
+ */
+
+
+function API(client)
+{  
   function apiHandle(res){
     return function(err, resp, json){
       if(err) return res.status(500).json(err)
@@ -7,10 +22,9 @@ function API(client){
     }
   }
   
-  this.auth   = function(req, res){ client.getRequestToken(req, res) }
-  this.access = function(req, res){ client.getAccessToken(req, res) }
-  
-  this.userAction = function(req, res){ client.user(req.params.action, req, apiHandle(res)) }
+  this.auth       = function(req, res){ client.getRequestToken(req, res) }
+  this.access     = function(req, res){ client.getAccessToken(req, res) }
+  this.userAction = function(req, res){ client.userRequest(req.params.action, req, apiHandle(res)) }
 }
 
 exports.API = API;

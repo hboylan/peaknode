94bbeb0003a0ae91420f3aeb8e59e20a8b9fa426
bluehugh2 <bluehugh2@gmail.com>
2013-08-15 13:47:51 -0400

    authenticated

--- routes.js
+++ routes.js
@@ -13,8 +13,7 @@ module.exports = function(app, sessions, db, omni_client, fit_client, xbmc_clien
     return function(req, res){
       var sessID;
       if(req.method == 'POST')      sessID = req.body.sessionID
-      else if(req.method == 'GET')  sessID = req.params.sessionID
-      console.log(req.method)
+      else if(req.method == 'GET')  sessID = req.query.sessionID
       console.log(sessID)
       if(callback == users.login || callback == users.logout){
         if(sessID) sessions.destroy(sessID, function(err, sess){})

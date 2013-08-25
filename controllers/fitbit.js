db958f27f544b67efb916942b192abd6bb7c590e
bluehugh2 <bluehugh2@gmail.com>
2013-08-24 21:24:56 -0400

    fitbit update

--- controllers/fitbit.js
+++ controllers/fitbit.js
@@ -33,7 +33,7 @@ function API(db, client)
     // Request token
     client.oauth.getOAuthRequestToken(function (error, token, secret, authorize_url, other) {
       if(error) return res.status(400).json({ error:'Failed to request token' })
-      res.redirect('http://www.fitbit.com/oauth/authorize?oauth_token=' + token + '&sessionID=' + sessID)
+      res.redirect('http://www.fitbit.com/oauth/authorize?oauth_token=' + token)
     })
   }
   this.access = function(sess, req, res){

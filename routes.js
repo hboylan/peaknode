c5f7ec34988957edd7c815343d33d444ab2a431c
bluehugh2 <bluehugh2@gmail.com>
2013-09-13 14:59:20 -0400

    xbmc

--- routes.js
+++ routes.js
@@ -124,6 +124,7 @@ module.exports = function(app, sessions, db, omni_client, fit_client, xbmc_clien
   app.get('/xbmc/dir', xbmc.dir)
   app.get('/xbmc/scan', xbmc.scan)
   app.get('/xbmc/songs', xbmc.songs)
+  app.get('/xbmc/songs/:id', xbmc.song)
   app.get('/xbmc/movies', xbmc.movies)
   app.get('/xbmc/movies/:id([0-9]+)', xbmc.movie)
   app.get('/xbmc/artists', xbmc.artists)

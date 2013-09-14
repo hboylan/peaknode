1ed5d3dd382d2247efe17067caa2d53692dce489
bluehugh2 <bluehugh2@gmail.com>
2013-09-14 15:23:00 -0400

    xbmc

--- controllers/appliances.js
+++ controllers/appliances.js
@@ -30,8 +30,7 @@ function ApplianceAPI(db, vera)
 
   this.switch = function(req, res) {
     var state = req.body.state
-      , node  = req.body.node;
-    req.body.type = 'switch';
+      , node  = req.body.node
     db.appliance.find(req.params.id).success(function(a){
       if(a == undefined) return res.status(401).json({ error:'Invalid zone' });
       //Update Audio Zone state
@@ -46,6 +45,7 @@ function ApplianceAPI(db, vera)
         if(node == 'left')        a.leftOn  = state == 'on'? true:false
         else if(node == 'right')  a.rightOn = state == 'on'? true:false
         a.save()
+        req.body.type = 'switch'
         vera.state(req, res)
       }else res.status(401)
     })

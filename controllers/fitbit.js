3cf5e022e59d6b6f68769834e35f3e21ef03d1c6
bluehugh2 <bluehugh2@gmail.com>
2013-09-26 12:06:55 -0700

    fitbits

--- controllers/fitbit.js
+++ controllers/fitbit.js
@@ -52,7 +52,7 @@ function API(db, client)
   this.hasToken = function(id, req, res){
     db.user.find(id).success(function(u){
       if(u && u.fitbit_token && u.fitbit_secret) res.send()
-      else res.json({ error:'Requires Fitbit access' })
+      else res.json({ success:false })
     })
   }
   
